e8368453138c863631e7bf800485ddbddb5a7f97
rydnr <github@acm-sl.org>
2024-01-17 19:22:27 +0100

    Added vim and emacs settings

--- pythoneda/shared/artifact/application/local_artifact_app.py
+++ pythoneda/shared/artifact/application/local_artifact_app.py
@@ -56,3 +56,11 @@ class LocalArtifactApp(PythonEDA, abc.ABC):
         :rtype: type[Artifact]
         """
         pass
+# vim: syntax=python ts=4 sw=4 sts=4 tw=79 sr et
+# Local Variables:
+# mode: python
+# python-indent-offset: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# fill-column: 79
+# End:
